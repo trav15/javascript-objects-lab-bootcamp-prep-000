7a5d65e849179395f0ffaf7a654ff14053bf88aa
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-11 14:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object[key] = value
+}
