7b944293e543c7f6cce7381e677fefc2b8e48139
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-11 14:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   console.log(newObj)
-  delete newObj.key
+  delete newObj[key]
   console.log(newObj)
   return newObj
 }
